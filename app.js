dc17ea16e1a3b7f23e8b9a8989f88eb299a2a9e6
barakafrancis <115995930+barakafrancis@users.noreply.github.com>
2025-05-30 19:02:52 +0300

    Update app.js
    port update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { OpenAI } = require('openai');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3002;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
